41962fb059556e0e8022ec4f0dbbdf3d8c03af34
Arnau Diaz <arnau.diaz@adevinta.com>
2022-02-17 15:41:35 +0100

    basic bar test with local config in test package

---
+++ src/test/java/bar/BarTestLocalConfig.java
@@ -0,0 +1,41 @@
+package bar;
+
+import foo.Foo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@ContextConfiguration(classes = BarTestLocalConfig.BarConfigTest.class)
+class BarTestLocalConfig {
+
+  @Configuration
+  @ComponentScan("bar")
+  static class BarConfigTest {
+
+  }
+
+  @Autowired
+  Bar bar;
+
+  @Test
+  void testsWorking() {
+    assertTrue(true);
+  }
+
+  @Test
+  void fooNotNull() {
+    assertThat(bar).isNotNull();
+  }
+
+  @Test
+  void fooName() {
+    assertThat(bar.name).isEqualTo("hello-bar-test");
+  }
+}
